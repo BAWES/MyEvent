44f6704bb80369fecc72778035adf261b8e572f3
Saoud AlTurki <saoudalturki@gmail.com>
2019-12-30 09:50:01 +0300

    Modifiy migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -15,6 +15,49 @@ class m130524_201442_init extends Migration
         /**
          * Create Admin Table
          */
+        $this->createTable('{{%admin}}', [
+            'admin_id' => $this->primaryKey(),
+            'admin_name' => $this->string()->notNull()->unique(),
+            'admin_email' => $this->string()->notNull()->unique(),
+            'admin_auth_key' => $this->string(32)->notNull(),
+            'admin_password_hash' => $this->string()->notNull(),
+            'admin_password_reset_token' => $this->string()->unique(),
+            'admin_status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'admin_created_at' => $this->datetime()->notNull(),
+            'admin_updated_at' => $this->datetime()->notNull(),
+        ], $tableOptions);
+        
+        // Add Saoud as Base Admin
+        $sql = 'INSERT INTO admin SET
+            admin_id = 1,
+            admin_name = "Saoud AlTurki",
+            admin_email = "saoud@bawes.net",
+            admin_auth_key = "Lu4vPW4Npfgce6WkXdt9OErpxXdB7GW4",
+            admin_password_hash = "$2y$13$LdNaUZOdyyL5.TYl/tbfI.i9YVkhxFd/9LTzaaCFgn4lCeTNgL8le",
+            admin_password_reset_token = NULL,
+            admin_status = 10,
+            admin_created_at = "2018-08-21 19:20:58",
+            admin_updated_at = "2018-08-21 19:40:58"
+            ';
+        Yii::$app->db->createCommand($sql)->execute();
+        
+        // Add Khalid as Base Admin
+        $sql = 'INSERT INTO admin SET
+            admin_id = 2,
+            admin_name = "Khalid AlMutawa",
+            admin_email = "khalid@bawes.net",
+            admin_auth_key = "Lu4vPW4Npfgce6WkXdt9OErpxXdB7GW4",
+            admin_password_hash = "$2y$13$uJ2Gcw66dRJ7G/S0V3e6turULkLnJjldzgnapvH0t.csMpWcOePgi",
+            admin_password_reset_token = NULL,
+            admin_status = 10,
+            admin_created_at = "2018-08-21 19:20:58",
+            admin_updated_at = "2018-08-21 19:40:58"
+            ';
+        Yii::$app->db->createCommand($sql)->execute();
+        
+        /**
+         * Create User Table
+         */
         $this->createTable('{{%user}}', [
             'user_uuid' => $this->primaryKey(),
             'user_name' => $this->string()->notNull()->unique(),
@@ -23,8 +66,8 @@ class m130524_201442_init extends Migration
             'user_password_hash' => $this->string()->notNull(),
             'user_password_reset_token' => $this->string()->unique(),
             'user_status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'user_created_at' => $this->integer()->notNull(),
-            'user_updated_at' => $this->integer()->notNull(),
+            'user_created_at' => $this->datetime()->notNull(),
+            'user_updated_at' => $this->datetime()->notNull(),
         ], $tableOptions);
         
          /**
@@ -122,7 +165,37 @@ class m130524_201442_init extends Migration
              'CASCADE'
          );
          
+        /**
+         * Create Venue Photo Table
+         * Photos of a venue
+         */
+         $this->createTable('{{%venue_photo}}', [
+            'photo_uuid' => $this->primaryKey(),
+            'venue_uuid' => $this->integer()->notNull(),
+            'photo_url' => $this->string(),
+            'venue_uuid' => $this->integer()->notNull(),
+            'photo_created_datetime' => $this->datetime()->notNull(),
+
+        ], $tableOptions);
+         
                  
+        // creates index for column `venue_uuid` in venue_photo table
+         $this->createIndex(
+             'idx-venue-photo-venue_uuid',
+             'venue_photo',
+             'venue_uuid'
+         );
+
+         // add foreign key for `venue_uuid` in table `venue_photo`
+         $this->addForeignKey(
+             'fk-venue-photo-venue_uuid',
+             'venue_photo',
+             'venue_uuid',
+             'venue',
+             'venue_uuid',
+             'CASCADE'
+         );
+         
     }
 
     public function down()
@@ -140,7 +213,7 @@ class m130524_201442_init extends Migration
         );
         
    
-                // drops ForeignKey for column `venue_uuid`
+        // drops ForeignKey for column `venue_uuid`
         $this->dropForeignKey(
             'fk-venue-occasion-venue_uuid',
             'venue_occasion'
@@ -152,7 +225,7 @@ class m130524_201442_init extends Migration
             'venue_occasion'
         );
         
-    // drops ForeignKey for column `occasion_uuid`
+        // drops ForeignKey for column `occasion_uuid`
         $this->dropForeignKey(
             'fk-venue-occasion-occasion_uuid',
             'venue_occasion'
@@ -165,14 +238,24 @@ class m130524_201442_init extends Migration
         );
         
     
+        // drops ForeignKey for column `venue_uuid`
+        $this->dropForeignKey(
+            'fk-venue-photo-venue_uuid',
+            'venue_photo'
+        ); 
         
+        // drops index for column `venue_uuid`
+        $this->dropIndex(
+            'idx-venue-photo-venue_uuid',
+            'venue_photo'
+        );
         
+        //Drop all tables
+        $this->dropTable('{{%admin}}');
         $this->dropTable('{{%user}}');
-        
         $this->dropTable('{{%occasion}}');
-        
         $this->dropTable('{{%venue}}');
-        
         $this->dropTable('{{%venue_occasion}}');
+        $this->dropTable('{{%venue_photo}}');
     }
 }
